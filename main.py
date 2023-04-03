a8a7550156e68240d4b88a410f061cc033ba8d75
matyd <maty.demjan@seznam.cz>
2023-04-03 15:18:54 +0200

    hotovson

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+
 meta = MetaData()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recept.db'
 db = SQLAlchemy(app)
+
 
 class Recept(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,25 +24,46 @@ class Recept(db.Model):
 def index():
     return render_template('index.html')
 
+
 @app.route('/recepty')
 def receptyy():
     recepty = Recept.query.all()
     return render_template('recepty.html', recepty=recepty)
 
-@app.route('/pridat_recept', methods=['GET','POST'])
+@app.route('/delete/<int:id>', methods=['GET', 'DELETE'])
+def delete_product(id):
+    product = Recept.query.get(id)
+    if not product:
+        return 'Recept nenalezen', 404
+    db.session.delete(product)
+    db.session.commit()
+    return 'Recept uspesne smazan'
+
+
+@app.route('/pridat_recept', methods=['GET', 'POST'])
 def pridat_recept():
     if request.method == 'POST':
         nazev = request.form['nazev']
         text = request.form['text']
         path = request.files['path']
         filename = path.filename
-        path.save(f'templates/pictures/{filename}')
+        # path.save(f'templates/pictures/{filename}')
+        path.save(f'./{filename}')
         ingredience = request.form['ingredience']
-        recept = Recept(nazev=nazev, text=text, ingredience=ingredience, path=f'./templates/pictures/{filename}')
+        recept = Recept(nazev=nazev, text=text, ingredience=ingredience,
+                        # path=f'C:/Users/matyd/PycharmProjects/piton-seminarni-arbeit/{filename}')
+                        path=f'../image/{filename}')
         db.session.add(recept)
         db.session.commit()
         return 'Recept uspesne pridan!'
     return render_template('pridat_recept.html')
+
+
+@app.route('/image/<string:custom_url>')
+def get_image(custom_url):
+    filename = f'{custom_url}'
+    return send_file(filename, mimetype='image/jpg')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
